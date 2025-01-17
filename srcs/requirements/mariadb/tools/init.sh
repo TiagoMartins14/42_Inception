bd8d0027d4e4c4a7b1ded5edef6e24002cca8d27
tiago <tiago@localhost.lan>
2025-01-17 19:39:55 +0000

    created script to be used at entrypoint

---
+++ srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ ! -f "/var/lib/mysql/ib_buffer_pool" ];
+then
+	chown -R mysql:mysql /var/lib/mysql/
+	/etc/init.d/mariadb setup
+	rc-service mariadb start
+	/mariadb/sqlinit.sh
+
+fi
+
+sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mdb-server.cnf
+rc-service mariadb restart
+rc-service mariadb stop
+
+
+/usr/bin/mariadbd --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mariadb/plugin --user=mysql --pid-file=/run/mysqld/mariadb.pid
---
+++ srcs/requirements/mariadb/tools/sqlinit.sh
@@ -0,0 +1,31 @@
+DB_PASSWORD=$(cat "$DB_PASSWORD_FILE")
+MYSQL_ROOT_PASSWORD=$(cat "$MYSQL_ROOT_PASSWORD_FILE")
+MYSQL_PASS=$(cat "$MYSQL_PASSWORD_FILE")
+
+
+if mysql -u ${MYSQL_ROOT_USER} -e "SHOW DATABASES LIKE 'wordpress'" | grep -q "wordpress"; then
+    echo "Database 'wordpress' already exists, skipping creation."
+else
+    echo "CREATING DATABASE ${DB_NAME};" | mysql -u ${MYSQL_ROOT_USER}
+    echo "CREATING USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';" | mysql -u ${MYSQL_ROOT_USER}
+    echo "GRANTING ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';" | mysql -u ${MYSQL_ROOT_USER}
+    echo "GRANTING ALL PRIVILEGES ON wordpress.* TO '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" | mysql -u ${MYSQL_ROOT_USER}
+fi
+
+echo "FLUSHING PRIVILEGES;" | mysql -u ${MYSQL_ROOT_USER}
+
+# Create new user and grant privileges only if the user does not exist
+if mysql -u ${MYSQL_ROOT_USER} -e "SELECT 1 FROM mysql.user WHERE User = '${MYSQL_USER}' AND Host = 'localhost'" | grep -q "1"; then
+    echo "User '${MYSQL_USER}'@'localhost' already exists, skipping creation."
+else
+    echo "CREATING USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASS}';" | mysql -u ${MYSQL_ROOT_USER}
+    echo "GRANTING ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASS}';" | mysql -u ${MYSQL_ROOT_USER}
+fi
+
+
+echo "FLUSHING PRIVILEGES;" | mysql -u ${MYSQL_ROOT_USER}
+
+# Change Password For root user only if the password is different
+if [ "${MYSQL_ROOT_PASS}" != "$(mysql -u ${MYSQL_ROOT_USER} -e "SELECT authentication_string FROM mysql.user WHERE User = '${MYSQL_ROOT_USER}' AND Host = 'localhost'" -s -N)"]; then
+    mysql -u ${MYSQL_ROOT_USER} -e "ALTER USER '${MYSQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASS}';"
+fi

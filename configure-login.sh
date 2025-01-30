1c53ed382c10f76c11abbb8e076d554385c6d868
tiaferna42 <tiaferna42@localhost.42porto.com>
2025-01-30 14:58:28 +0000

    updates nginx Dockerfile login

--- configure-login.sh
+++ configure-login.sh
@@ -3,4 +3,5 @@
 # LOGIN variable need to be edited in Makefile first
 
 sed -i "s/login/${LOGIN}/g" srcs/.env
+sed -i "s/login/${LOGIN}/g" srcs/requirements/nginx/Dockerfile
 sed -i "s/login/${LOGIN}/g" srcs/requirements/nginx/conf/default.conf
